f9e8f64953cdee7b5fb5a951c881990e1edec71a
Daniel Gonzalez <daniel_gonzalezc@yahoo.com>
2019-12-23 09:59:34 -0500

    Upload ChatJonsity.Test
    For nUnit test

---
+++ GetStockTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ChatJobsity.Tests
+{
+    [TestClass]
+    public class GetStockTest
+    {
+        [TestMethod]
+        public void GetStockApple()
+        {
+            string _stockCode = "AAPL.US";
+            string _respuesta=new ChatJobsity.App_Code.StockProvider().GetStock(_stockCode);
+            string _getCodigo = _respuesta.Split(',')[0];
+            Assert.AreEqual(_stockCode, _getCodigo);
+
+
+
+        }
+    }
+}
